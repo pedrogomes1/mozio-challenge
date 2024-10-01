1b4cafbc502324dd6e92d44a5caacdec14cefdf2
Pedro Gomes <pedro-gomes@ibm.com>
2024-10-01 20:24:27 -0300

    feat: config main app with redux and react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
+import { store } from './app/store'
 
 import './index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 )
