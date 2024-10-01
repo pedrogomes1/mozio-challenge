ad6bf13488016733f5f6b0045ebe79c2e5caa1ed
Pedro Gomes <pedro-gomes@ibm.com>
2024-10-01 20:22:40 -0300

    config: adjust all config projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
   },
-  plugins: [],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
