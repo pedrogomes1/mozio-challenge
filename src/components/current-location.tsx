187096b22a327010db8718476d3a62325f38229f
Pedro Gomes <pedoguinha2@gmail.com>
2024-10-02 02:01:23 -0300

    feat: improvement list infos of current location

--- src/components/current-location.tsx
+++ src/components/current-location.tsx
@@ -4,25 +4,24 @@ import { NearbyLocations } from './nearby-locations'
 export function CurrentLocation() {
   const location = useAppSelector(state => state.location)
 
+  const currentLocationInformationList = [
+    { title: 'Country', value: location.country },
+    { title: 'Climate', value: location.climate },
+    { title: 'Currency', value: location.currency },
+  ]
+
   return (
-    <section>
-      <p>{location.name}</p>
-      <p>{location.description}</p>
-      <ul>
-        <li className="font-bold">
-          <span>Country:</span> <span>{location.country}</span>
-        </li>
-
-        <li className="font-bold">
-          <span>Climate:</span> <span>{location.climate}</span>
-        </li>
-
-        <li className="font-bold">
-          <span>Currency:</span> <span>{location.currency}</span>
-        </li>
+    <section className="mt-10">
+      <p className="text-3xl font-normal">{location.name}</p>
+      <p className="my-4 text-xl">{location.description}</p>
+      <ul className="flex flex-col gap-3">
+        {currentLocationInformationList.map(location => (
+          <li key={location.value} className="text-lg">
+            <span className="font-extrabold">{location.title}:</span>{' '}
+            <span>{location.value}</span>
+          </li>
+        ))}
       </ul>
-      <h1 className="mt-10 mb-10">Nearby Locations</h1>
-
       <NearbyLocations />
     </section>
   )
